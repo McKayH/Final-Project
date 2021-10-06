eadef8ff831c636d45eb63c029f8de67bcd526ba
McKayH <mckay.hilmo1766@stu.mtec.edu>
2021-10-06 12:06:31 -0600

    First commit

---
+++ Desktop/ProjectsForIntro/JqueryProWitnDarMode/app.js
@@ -0,0 +1,88 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+let theAppend;
+let theInfo = $('#theInformation');
+
+for(let i = 0; i < storeItems.length; i++)
+{
+    theAppend = storeItems[i];
+    if(theAppend.inStock)
+    {
+        $(theInfo).append('<div class="item"<p>' 
+        + '<div class="price">' +'$'+theAppend.price +'</div>'
+        +'<div class="name">'+ theAppend.name +'</div>'
+        +'<div class="details">'+ theAppend.details + '</div>' 
+        + '</p></div>');
+    }
+}
+$('#theButton').click(function()
+{
+    $('#Background, #smallBorder, #theProducts, #theButton, .item').toggleClass('darkmode');
+});
